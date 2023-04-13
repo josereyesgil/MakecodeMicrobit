b6a27e570b384c91adb55e7f70f689a199288a68
josereyesgil <60502883+josereyesgil@users.noreply.github.com>
2023-04-13 20:19:25 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
-basic.showString("¡HOLA MARTINA!")
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        music.playMelody("C C D C F E - - ", 100)
+    } else {
+        if (input.buttonIsPressed(Button.B)) {
+            basic.showString("SOMOS LA MEJOR FAMILIA!")
+        } else {
+            if (input.buttonIsPressed(Button.AB)) {
+                music.startMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once)
+            } else {
+                basic.showIcon(IconNames.Heart)
+                basic.pause(100)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.pause(100)
+                basic.showIcon(IconNames.Happy)
+                basic.pause(100)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+            }
+        }
+    }
+})
